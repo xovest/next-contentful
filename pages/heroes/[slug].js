c50795727d6d40daf0d086deb34e7103d4e72655
xovest <79008917+xovest@users.noreply.github.com>
2021-12-04 13:46:15 +0200

    fallback not guys

--- pages/heroes/[slug].js
+++ pages/heroes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false 
+    fallback: true 
   };
 };
 
@@ -37,6 +38,7 @@ export async function getStaticProps({ params }) {
 };
 
 export default function RecipeDetails({ hero }) {
+  if (!hero) return <Skeleton />
   const { featuredImage, title, cookingTime, ingredients, method } = hero.fields;
 
   return (
