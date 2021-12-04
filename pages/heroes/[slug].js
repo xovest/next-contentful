6d3a21ed1267f88ba332b7bc47c9256cd79d9620
xovest <79008917+xovest@users.noreply.github.com>
2021-12-04 12:05:45 +0200

    updated each show more

--- pages/heroes/[slug].js
+++ pages/heroes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import Image from 'next/image'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -34,10 +36,57 @@ export async function getStaticProps({ params }) {
 };
 
 export default function RecipeDetails({ hero }) {
-  console.log(hero);
+  const { featuredImage, title, cookingTime, ingredients, method } = hero.fields;
+
   return (
     <div>
-      Hero Details
+      <div className="banner">
+        <Image 
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{ title }</h2>
+      </div>
+
+      <div className="info">
+        <p>Dmg: { cookingTime }</p>
+        <h3>Some atts of hero:</h3>
+        {ingredients.map(ing => (
+          <span key={ing}>{ ing }</span>
+        ))}
+      </div>
+
+      <div className="method">
+        <h3>Details about the hero:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx>{`
+        h2,h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
     </div>
   )
 }
